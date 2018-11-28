5252a4270d7799212e909b1ba91e139685fb5712
wajidsnaa <wajidsnaakhan@gmail.com>
2018-11-28 09:48:46 +0000

    Be-Xtended

--- sdm845.mk
+++ sdm845.mk
@@ -13,8 +13,8 @@ $(call inherit-product-if-exists, vendor/xiaomi/sdm845-common/sdm845-common-vend
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
+#    $(LOCAL_PATH)/overlay-lineage
 
 # Properties
 -include $(LOCAL_PATH)/common-props.mk
