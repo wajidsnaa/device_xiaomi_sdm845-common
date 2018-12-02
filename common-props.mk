f1d7a425bea92e24b8da62f34f8506ad14bdbe42
ganeshi4u <ganeshvarmai4u@gmail.com>
2018-12-02 11:02:30 +0100

    sdm845-common: enable ViLTE prop
    Change-Id: I290c236913244720d641454d5f4e8e1cb8ae9d8d
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- common-props.mk
+++ common-props.mk
@@ -159,7 +159,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.rcs.supported=0 \
-    persist.vendor.ims.disableUserAgent=0
+    persist.vendor.ims.disableUserAgent=0 \
+    persist.radio.calls.on.ims=1
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
